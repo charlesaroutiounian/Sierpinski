9d552d79b645ed1e9a0577ed1efa11486584e6bb
Charles Aroutiounian <charlesaroutiounian@me.com>
2024-04-03 14:01:02 +0900

    Comments
    Added comments and modified the canvas for drawing the Sierpinski Triangle

--- Sierpinski.playground/Contents.swift
+++ Sierpinski.playground/Contents.swift
@@ -14,7 +14,7 @@ class DrawingView: UIView {
     override func draw(_ rect: CGRect) {
         super.draw(rect)
         
-        // Drawing code here.
+        // Drawing the main triangle
         let path = UIBezierPath(rect: rect)
         path.move(to: CGPoint(x: 0, y: 0))
         path.addLine(to: CGPoint(x: self.frame.size.width/2, y: self.frame.size.height))
@@ -22,11 +22,14 @@ class DrawingView: UIView {
         path.lineWidth = 1.0
         path.stroke()
         
+        // Calling function to draw smaller triangles recursively
         subTriangle(1, x1: (0 + self.frame.size.width/2) / 2, y1: (0 + self.frame.size.height) / 2, x2: (0 + self.frame.size.width) / 2, y2: (0 + 0) / 2, x3: (self.frame.size.width/2 + self.frame.size.width) / 2, y3: ( self.frame.size.height + 0) / 2)
     }
     
+    // Function to draw smaller triangles recursively
     func subTriangle(_ n: Int, x1: CGFloat, y1: CGFloat, x2: CGFloat, y2: CGFloat, x3: CGFloat ,y3: CGFloat) {
-        // Drawing code here.
+       
+        // Drawing code here
         let path = UIBezierPath()
         path.move(to: CGPoint(x: x1, y: y1))
         path.addLine(to: CGPoint(x: x2, y: y2))
@@ -34,24 +37,21 @@ class DrawingView: UIView {
         path.lineWidth = 1.0
         path.stroke()
         
+        // Recursively draw smaller triangles until n reaches a certain limit
         if (n<10){
-            
             subTriangle(n+1, x1: (x1 + x2) / 2 + (x2 - x3) / 2, y1: (y1 + y2) / 2 + (y2 - y3) / 2, x2: (x1 + x2) / 2 + (x1 - x3) / 2, y2:(y1 + y2) / 2 + (y1 - y3) / 2, x3:(x1 + x2) / 2, y3:(y1 + y2) / 2 )
-            
             subTriangle(n+1, x1: (x3 + x2) / 2 + (x2 - x1) / 2, y1: (y3 + y2) / 2 + (y2 - y1) / 2, x2: (x3 + x2) / 2 + (x3 - x1) / 2, y2:(y3 + y2) / 2 + (y3 - y1) / 2, x3: (x3 + x2) / 2, y3:(y3 + y2) / 2 )
-            
             subTriangle(n+1, x1: (x1 + x3) / 2 + (x3 - x2) / 2, y1: (y1 + y3) / 2 + (y3 - y2) / 2, x2: (x1 + x3) / 2 + (x1 - x2) / 2, y2:(y1 + y3) / 2 + (y1 - y2) / 2, x3: (x1 + x3) / 2, y3:(y1 + y3) / 2 )
         }
-        
-        
     }
 }
 
-let drawingView = DrawingView(frame: CGRect(x: 0, y: 0, width: 500, height: 500))
+// Creating and configuring the drawing view
+let drawingView = DrawingView(frame: CGRect(x: 0, y: 0, width: 500, height: 375))
 drawingView.layer.transform = CATransform3DMakeRotation(CGFloat.pi, 0, 0, 1)
-
 drawingView.backgroundColor = .white
 
-
+// Setting up the view in Playground
 PlaygroundPage.current.liveView = drawingView
+
 
