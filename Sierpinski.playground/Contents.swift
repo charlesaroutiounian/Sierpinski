0fc53c524e3a8f64eee1d366c46566d48615d686
Charles Aroutiounian <charlesaroutiounian@me.com>
2024-05-04 11:18:59 +0900

    Added Drawing Animations
    Drawing Animations have been added

--- Sierpinski.playground/Contents.swift
+++ Sierpinski.playground/Contents.swift
@@ -3,6 +3,12 @@ import PlaygroundSupport
 
 class DrawingView: UIView {
     
+    var drawingLimit: Int = 0 {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
@@ -19,16 +25,15 @@ class DrawingView: UIView {
         path.move(to: CGPoint(x: 0, y: 0))
         path.addLine(to: CGPoint(x: self.frame.size.width/2, y: self.frame.size.height))
         path.addLine(to: CGPoint(x: self.frame.size.width, y: 0))
-        path.lineWidth = 1.0
+        path.lineWidth = 1
         path.stroke()
         
         // Calling function to draw smaller triangles recursively
-        subTriangle(1, x1: (0 + self.frame.size.width/2) / 2, y1: (0 + self.frame.size.height) / 2, x2: (0 + self.frame.size.width) / 2, y2: (0 + 0) / 2, x3: (self.frame.size.width/2 + self.frame.size.width) / 2, y3: ( self.frame.size.height + 0) / 2)
+        subTriangle(level: 1, x1: (0 + self.frame.size.width/2) / 2, y1: (0 + self.frame.size.height) / 2, x2: (0 + self.frame.size.width) / 2, y2: (0 + 0) / 2, x3: (self.frame.size.width/2 + self.frame.size.width) / 2, y3: ( self.frame.size.height + 0) / 2)
     }
     
     // Function to draw smaller triangles recursively
-    func subTriangle(_ n: Int, x1: CGFloat, y1: CGFloat, x2: CGFloat, y2: CGFloat, x3: CGFloat ,y3: CGFloat) {
-       
+    func subTriangle(level: Int, x1: CGFloat, y1: CGFloat, x2: CGFloat, y2: CGFloat, x3: CGFloat ,y3: CGFloat) {
         // Drawing code here
         let path = UIBezierPath()
         path.move(to: CGPoint(x: x1, y: y1))
@@ -37,11 +42,11 @@ class DrawingView: UIView {
         path.lineWidth = 1.0
         path.stroke()
         
-        // Recursively draw smaller triangles until n reaches a certain limit
-        if (n<10){
-            subTriangle(n+1, x1: (x1 + x2) / 2 + (x2 - x3) / 2, y1: (y1 + y2) / 2 + (y2 - y3) / 2, x2: (x1 + x2) / 2 + (x1 - x3) / 2, y2:(y1 + y2) / 2 + (y1 - y3) / 2, x3:(x1 + x2) / 2, y3:(y1 + y2) / 2 )
-            subTriangle(n+1, x1: (x3 + x2) / 2 + (x2 - x1) / 2, y1: (y3 + y2) / 2 + (y2 - y1) / 2, x2: (x3 + x2) / 2 + (x3 - x1) / 2, y2:(y3 + y2) / 2 + (y3 - y1) / 2, x3: (x3 + x2) / 2, y3:(y3 + y2) / 2 )
-            subTriangle(n+1, x1: (x1 + x3) / 2 + (x3 - x2) / 2, y1: (y1 + y3) / 2 + (y3 - y2) / 2, x2: (x1 + x3) / 2 + (x1 - x2) / 2, y2:(y1 + y3) / 2 + (y1 - y2) / 2, x3: (x1 + x3) / 2, y3:(y1 + y3) / 2 )
+        // Recursively draw smaller triangles until level reaches the drawing limit
+        if level < drawingLimit {
+            subTriangle(level: level+1, x1: (x1 + x2) / 2 + (x2 - x3) / 2, y1: (y1 + y2) / 2 + (y2 - y3) / 2, x2: (x1 + x2) / 2 + (x1 - x3) / 2, y2:(y1 + y2) / 2 + (y1 - y3) / 2, x3:(x1 + x2) / 2, y3:(y1 + y2) / 2 )
+            subTriangle(level: level+1, x1: (x3 + x2) / 2 + (x2 - x1) / 2, y1: (y3 + y2) / 2 + (y2 - y1) / 2, x2: (x3 + x2) / 2 + (x3 - x1) / 2, y2:(y3 + y2) / 2 + (y3 - y1) / 2, x3: (x3 + x2) / 2, y3:(y3 + y2) / 2 )
+            subTriangle(level: level+1, x1: (x1 + x3) / 2 + (x3 - x2) / 2, y1: (y1 + y3) / 2 + (y3 - y2) / 2, x2: (x1 + x3) / 2 + (x1 - x2) / 2, y2:(y1 + y3) / 2 + (y1 - y2) / 2, x3: (x1 + x3) / 2, y3:(y1 + y3) / 2 )
         }
     }
 }
@@ -54,4 +59,15 @@ drawingView.backgroundColor = .white
 // Setting up the view in Playground
 PlaygroundPage.current.liveView = drawingView
 
+// Timer to incrementally increase the drawing limit
+var currentLimit = 0
+let maxLimit = 10
+let timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+    drawingView.drawingLimit = currentLimit
+    currentLimit += 1
+    if currentLimit > maxLimit {
+        timer.invalidate()
+    }
+}
+RunLoop.current.add(timer, forMode: .default)
 
